e282b131af101c5733ad4311b470917685b4369c
Kai Mindermann <kai.mindermann@informatik.uni-stuttgart.de>
2018-10-17 16:11:49 +0200

    template release

---
+++ app/src/androidTest/java/de/uni_stuttgart/informatik/sopra/sopraapp/ExampleInstrumentedTest.java
@@ -0,0 +1,45 @@
+package de.uni_stuttgart.informatik.sopra.sopraapp;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+
+    @Test
+    public void useAppContext() throws Exception {
+
+        MainActivity activity = rule.getActivity();
+
+        TextView tvHello = (TextView) activity.findViewById(R.id.tvHello);
+        assertThat(tvHello,notNullValue());
+        assertThat(tvHello, instanceOf(TextView.class));
+        assertEquals("TextView text compare", "Hello World!", tvHello.getText().toString());
+
+        Button btnButton =(Button) activity.findViewById(R.id.btnButton);
+        assertThat(btnButton,notNullValue());
+        assertEquals("Test button text", "mein Button", btnButton.getText().toString());
+
+    }
+}
---
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
@@ -0,0 +1,38 @@
+package de.uni_stuttgart.informatik.sopra.sopraapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvHelloRef;
+    Button btnButtonRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvHelloRef = (TextView) findViewById(R.id.tvHello);
+        btnButtonRef = (Button) findViewById(R.id.btnButton);
+
+        btnButtonRef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), DisplayActivity.class);
+                String message = tvHelloRef.getText().toString();
+                intent.putExtra("Message", message);
+                startActivity(intent);
+
+//                TextDialog dialog = new TextDialog();
+//                dialog.show(getFragmentManager(), TextDialog.class.getSimpleName());
+            }
+        });
+
+
+    }
+}
