76f27d77b466cf25eda0187583f355e2f5adf50e
Samuel Gigliotti <st157598@stud.uni-stuttgart.de>
2018-11-20 18:10:10 +0100

    started coding. Finished a basic QR-Code Scanner with zxing-android-embedded

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
@@ -1,38 +1,38 @@
 package de.uni_stuttgart.informatik.sopra.sopraapp;
 
+
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tvHelloRef;
-    Button btnButtonRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new IntentIntegrator(this).initiateScan();
+    }
 
-        tvHelloRef = (TextView) findViewById(R.id.tvHello);
-        btnButtonRef = (Button) findViewById(R.id.btnButton);
-
-        btnButtonRef.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), DisplayActivity.class);
-                String message = tvHelloRef.getText().toString();
-                intent.putExtra("Message", message);
-                startActivity(intent);
-
-//                TextDialog dialog = new TextDialog();
-//                dialog.show(getFragmentManager(), TextDialog.class.getSimpleName());
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                new IntentIntegrator(this).initiateScan();
             }
-        });
-
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
 
     }
 }
