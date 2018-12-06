c71cf0e4fc0c90f91d0db5464537d40ac44485d6
Samuel Gigliotti <st157598@stud.uni-stuttgart.de>
2018-12-06 13:05:00 +0100

    Update

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
@@ -1,10 +1,16 @@
 package de.uni_stuttgart.informatik.sopra.sopraapp;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
