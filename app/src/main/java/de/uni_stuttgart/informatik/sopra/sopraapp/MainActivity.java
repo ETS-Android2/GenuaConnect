a0e85b205c505c9d4835a28438de59b8660bccaa
Baran <st156502@stud.uni-stuttgart.de>
2019-01-18 19:02:34 +0100

    Hilfeklasse hinzugefügt

--- app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
+++ app/src/main/java/de/uni_stuttgart/informatik/sopra/sopraapp/MainActivity.java
@@ -4,6 +4,8 @@ package de.uni_stuttgart.informatik.sopra.sopraapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -68,6 +70,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
@@ -78,6 +87,12 @@ public class MainActivity extends AppCompatActivity {
             intentIntegrator.setOrientationLocked(false);
             intentIntegrator.setCaptureActivity(RotatingCaptureActivity.class);
             intentIntegrator.initiateScan();
+        }else if(id ==R.id.preference) {
+            Intent intent = new Intent(this, HelpActivity.class);
+            this.startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
         return super.onOptionsItemSelected(item);
     }
